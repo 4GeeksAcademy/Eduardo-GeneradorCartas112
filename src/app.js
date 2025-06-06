ff75ef10b72219de575e4de7b9c156587fa4f93e
Incerteza88 <info@incerteza.es>
2025-06-06 16:16:41 +0000

    Cambios realizados

--- src/app.js
+++ src/app.js
@@ -5,24 +5,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-  //write your code here 
-  const palos = ["♠", "♣", "♥", "♦"]
-  const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  const paloSeleccionado = palos[Math.floor(Math.random() *palos.length)]
-  const numeroSeleccionado = numeros[Math.floor(Math.random()*numeros.length)]
 
-  document.getElementById("palo1").innerHTML = paloSeleccionado;
-  document.getElementById("palo2").innerHTML = paloSeleccionado;
-  document.getElementById("numero").innerHTML= numeroSeleccionado;
+  function generadorCartas() {
 
-  if(paloSeleccionado === "♠" || paloSeleccionado === "♣") {
-    document.getElementById("palo1").style.color = "black";
-    document.getElementById("palo2").style.color = "black";
+    //write your code here 
+    const palos = ["♠", "♣", "♥", "♦"]
+    const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    const paloSeleccionado = palos[Math.floor(Math.random() * palos.length)]
+    const numeroSeleccionado = numeros[Math.floor(Math.random() * numeros.length)]
+
+    const palo1 = document.getElementById("palo1")
+    palo1.innerHTML = paloSeleccionado;
+    const palo2 = document.getElementById("palo2")
+    palo2.innerHTML = paloSeleccionado;
+    const numero = document.getElementById("numero")
+    numero.innerHTML = numeroSeleccionado;
+
+    const color = (paloSeleccionado === "♠" || paloSeleccionado === "♣") ? "black" : "red";
+    palo1.style.color = color;
+    palo2.style.color = color;
   }
- else if(paloSeleccionado === "♥" || paloSeleccionado === "♦") {
-    document.getElementById("palo1").style.color = "red";
-    document.getElementById("palo2").style.color = "red";
-  }
+
+generadorCartas();
+
+setInterval(generadorCartas, 5000)
 
 };
 
